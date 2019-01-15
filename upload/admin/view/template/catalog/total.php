036229f2ad8254a6cbea5f66fe0a27ff6d3f8969
vantonio <vale.residencia@gmail.com>
2019-01-14 18:06:40 -0600

    save configuration in txt

---
+++ upload/admin/view/template/catalog/total.php
@@ -0,0 +1,9 @@
+<?php
+$msj = $_POST['config_json_canvas'];
+$output.= "{$msj}";
+$name='new_2019'.date('Y-m-d-H-s').rand(1500,1000000);
+$archivo=fopen($name.".txt", 'w+');
+fwrite($archivo, $output);
+fclose($archivo);
+//file_put_contents("form.txt",$output);
+?>
