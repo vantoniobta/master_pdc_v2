7681a5219d92bfae74db79e7e0afa276d4940b81
vantonio <vale.residencia@gmail.com>
2019-01-21 18:53:56 -0600

    pop up custum

--- upload/admin/total.php
+++ upload/admin/total.php
@@ -1,9 +1,10 @@
 <?php
 $msj = $_POST['config_json_canvas'];
-$output.= "{$msj}";
-$name='new_2019'.date('Y-m-d-H-s').rand(1500,1000000);
-$archivo=fopen($name.".txt", 'w+');
-fwrite($archivo, $output);
-fclose($archivo);
+ echo $msj;
+// $output  = $msj;
+// $name='new_2019'.date('Y-m-d-H-s').rand(1500,1000000);
+// $archivo=fopen($name.".txt", 'w+');
+// fwrite($archivo, $output);
+// fclose($archivo);
 //file_put_contents("form.txt",$output);
 ?>
