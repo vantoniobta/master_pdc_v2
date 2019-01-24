99633f8fb0b7b88b707ea94af6c4d470285b6ccf
vantonio <vale.residencia@gmail.com>
2019-01-23 19:02:50 -0600

    formDataRequest admin save txt

--- upload/admin/total.php
+++ upload/admin/total.php
@@ -1,10 +1,8 @@
 <?php
 $msj = $_POST['config_json_canvas'];
- echo $msj;
-// $output  = $msj;
-// $name='new_2019'.date('Y-m-d-H-s').rand(1500,1000000);
-// $archivo=fopen($name.".txt", 'w+');
-// fwrite($archivo, $output);
-// fclose($archivo);
-//file_put_contents("form.txt",$output);
+$output = $msj;
+$name='new_2018'.date('Y-m-d-H-s').rand(1500,1000000);
+$archivo=fopen($name.".txt", 'w+');
+fwrite($archivo, $output);
+fclose($archivo);
 ?>
