1978d79cd56379455962bf9d4db4544272431dbb
vantonio <vale.residencia@gmail.com>
2019-02-15 13:24:28 -0600

    edit front custom css

--- upload/admin/total.php
+++ upload/admin/total.php
@@ -1,10 +1,10 @@
 <?php
 
-$msj     = $_POST['config_json_canvas'];
-echo $_POST['product_id_'];
-$output  = $msj;
-$name    = 'new_custom_'.date('Y-m-d-H-s').rand(1500,1000000);
-$archivo = fopen($name.".txt", 'w+');
+$msj         = $_POST['config_json_canvas'];
+$product_id_ = $_POST['product_id_']; //id del producto 
+$output      = $msj;
+$name        = 'new_custom_'.date('Y-m-d-H-s').rand(1500,1000000);
+$archivo     = fopen($name.".txt", 'w+');
 fwrite($archivo, $output);
 fclose($archivo);
 
